d632e7d20a4d7c35b78b74709efbe1551f3c191c
Jina Strickland <jina30@gmail.com>
2020-07-01 21:24:49 +0000

    Done.

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,7 +8,7 @@ row_index = 0
     element_index = 0
     low_temp_element = -1
   while element_index < src[row_index].length do
-    if low_temp_element < src[row_index][element_index]
+    if low_temp_element > src[row_index][element_index]
       low_temp_element = src[row_index][element_index]
     end
       element_index += 1
