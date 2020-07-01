21f59759ba100eae100828f87ac4fa666949c961
Jina Strickland <jina30@gmail.com>
2020-07-01 21:30:16 +0000

    Done.

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,7 +8,7 @@ row_index = 0
     element_index = 0
     low_temp_element = -1
   while element_index < src[row_index].length do
-    if low_temp_element > src[row_index][element_index]
+    if low_temp_element < src[row_index][element_index]
       low_temp_element = src[row_index][element_index]
     end
       element_index += 1
@@ -18,3 +18,34 @@ row_index = 0
   end
     low_temp_src
 end
+
+
+array_of_daily_temperatures = [
+  [19, 21, 24, 26, 30],
+  [17, 18, 22, 26, 31],
+  [13, 15, 20, 23, 33],
+  [19, 20, 25, 28, 30],
+  [21, 25, 28, 30, 35],
+  [25, 29, 30, 36, 39],
+  [30, 36, 38, 39, 40],
+]
+
+
+
+
+# low_temp_src = []
+# row_index = 0
+#   while row_index < src.count do
+#     element_index = 0
+#     low_temp_element = -1
+#   while element_index < src[row_index].length do
+#     if low_temp_element < src[row_index][element_index]
+#       low_temp_element = src[row_index][element_index]
+#     end
+#       element_index += 1
+#   end
+#     low_temp_src << low_temp_element
+#     row_index += 1
+#   end
+#     low_temp_src
+# end
