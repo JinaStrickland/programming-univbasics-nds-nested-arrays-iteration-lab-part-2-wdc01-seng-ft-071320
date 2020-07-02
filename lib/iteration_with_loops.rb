9dfb3d38525994801a05567df3ae082f094b33d5
Jina Strickland <jina30@gmail.com>
2020-07-02 01:55:39 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,9 +4,9 @@ low_temp_src = []
 row_index = 0
   while row_index < src.count do
     element_index = 0
-    max_value = nil
+    max_value = 1000
   while element_index < src[row_index].length do
-    if src[row_index][element_index] == max_value || src[row_index][element_index] < max_value
+    if src[row_index][element_index] = max_value
       max_value = src[row_index][element_index]
     end
       element_index += 1
