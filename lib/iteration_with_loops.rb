e918e231dc3823fdfbdc5c2878613bed0bb8dd79
Jina Strickland <jina30@gmail.com>
2020-07-02 01:48:12 +0000

    Done.

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,36 +1,21 @@
-def find_min_in_nested_arrays(src)
-  # src will be an array of arrays of integers
-  # Produce a new Array that contains the smallest number of each of the nested arrays
 
+def find_min_in_nested_arrays(src)
 low_temp_src = []
 row_index = 0
   while row_index < src.count do
     element_index = 0
-    low_temp_element = -1
+    max_value = 1000
   while element_index < src[row_index].length do
-    if low_temp_element < src[row_index][element_index]
-      low_temp_element = src[row_index][element_index]
+    if src[row_index][element_index] < max_value
+      max_value = src[row_index][element_index]
     end
       element_index += 1
   end
-    low_temp_src << low_temp_element
+    low_temp_src << max_value
     row_index += 1
   end
     low_temp_src
 end
-
-
-array_of_daily_temperatures = [
-  [19, 21, 24, 26, 30],
-  [17, 18, 22, 26, 31],
-  [13, 15, 20, 23, 33],
-  [19, 20, 25, 28, 30],
-  [21, 25, 28, 30, 35],
-  [25, 29, 30, 36, 39],
-  [30, 36, 38, 39, 40],
-]
-
-
 
 
 # low_temp_src = []
@@ -49,3 +34,44 @@ array_of_daily_temperatures = [
 #   end
 #     low_temp_src
 # end
+
+
+# def find_min_in_nested_arrays(src)
+# low_temp_src = []
+# row_index = 0
+#   while row_index < src.count do
+#     element_index = 0
+#     larger_value = 1000
+#   while element_index < src[row_index].length do
+#     if src[row_index][element_index] < src[row_index][-1]
+#       low_temp_element = src[row_index][element_index]
+#     end
+#       element_index += 1
+#   end
+#     low_temp_src << low_temp_element
+#     row_index += 1
+#   end
+#     low_temp_src
+# end
+
+
+# low_temp_src = []
+# row_index = 0
+#   while row_index < src.count do
+#     element_index = 0
+#     low_temp_element = -1
+#   while element_index < src[row_index].length do
+#     if low_temp_element < src[row_index][element_index]
+#       low_temp_element = src[row_index][element_index]
+#     end
+#       element_index += 1
+#   end
+#     low_temp_src << low_temp_element
+#     row_index += 1
+#   end
+#     low_temp_src
+# end
+
+
+# src will be an array of arrays of integers
+# Produce a new Array that contains the smallest number of each of the nested arrays
